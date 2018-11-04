8a8e6524780c5a3947d0742f34235db6ff3f283a
Juan Carlos Giraldo Cardozo <jgiraldocardozo@gmail.com>
2018-11-04 09:31:11 -0500

    es.ts
    A translation into Spanish to be used in the components

---
+++ packages/vuetify/src/locale/es.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Elementos por página:',
+    rowsPerPageAll: 'Todos',
+    pageText: '{0}-{1} of {2}',
+    noResultsText: 'No se encontraron registros que coincidieran',
+    nextPage: 'Página siguiente',
+    prevPage: 'Página Anterior'
+  },
+  dataTable: {
+    rowsPerPageText: 'Filas por página:'
+  },
+  noDataText: 'No hay datos disponibles'
+}
